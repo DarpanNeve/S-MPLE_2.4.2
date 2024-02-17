12ef852baa5480d67042ded5b48ad3dce3dd576d
Darpan <darpanneve3@gmail.com>
2024-02-17 22:19:04 +0530

    server error checking

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const app = express()
-const PORT = 8000
+const app = express();
+const PORT = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get('/about', (req, res) => {
-  res.send('About route 🎉 ')
-})
+app.get("/about", (req, res) => {
+  res.send("About route 🎉 ");
+});
 
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
-})
+});
