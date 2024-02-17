5afb4310d364576e49e29e0bc49e11040f7a6412
Viraj Balasaheb Patil <virajpatil818@gmail.com>
2024-02-17 23:16:06 +0530

    Emergency screen added

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:medi_connect/src/screens/appointmentscreen.dart';
+import 'package:medi_connect/src/screens/emergencyscreen.dart';
 import 'package:medi_connect/src/screens/hospitalscreen.dart';
 import 'package:medi_connect/src/screens/profile_page.dart';
 
@@ -18,7 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final List<Widget> _tabs = [
     const HospitalScreen(),
     const AppointmentScreen(),
-    const Placeholder(),
+    const EmergencyScreen(),
   ];
 
   @override
